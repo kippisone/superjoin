238a401847edf308d474724665b017a48befab8f
Andi <andi@noname-media.com>
2015-08-21 20:38:22 +0200

    Added bower support

--- superjoin.js
+++ superjoin.js
@@ -6,10 +6,48 @@ module.exports = (function() {
     
     var grunt = require('grunt');
     
-    var Superjoin = function() {
-        this.root = process.cwd();
+    var Superjoin = function(conf) {
+        conf = conf || {};
+
+        var dir;
+
+        this.root = conf.root || process.cwd();
         this.modules = [];
         this.confFiles = [];
+
+        this.libDir = conf.libDir || null;
+        this.bwrDir = conf.bwrDir || null;
+        this.npmDir = conf.npmDir || null;
+
+        if (!this.bwrDir) {
+            dir = path.join(process.cwd(), 'bower_components');
+            while (true) {
+                if (grunt.file.exists(dir)) {
+                    this.bwrDir = dir;
+                    break;
+                }
+
+                dir = path.join(dir, '../../bower_components');
+                if (dir === '/') {
+                    break;
+                }
+            }
+        }
+
+        if (!this.npmDir) {
+            dir = path.join(process.cwd(), 'node_modules');
+            while (true) {
+                if (grunt.file.exists(dir)) {
+                    this.npmDir = dir;
+                    break;
+                }
+
+                dir = path.join(dir, '../../node_modules');
+                if (dir === '/') {
+                    break;
+                }
+            }
+        }
     };
 
     Superjoin.prototype.join = function(files, main) {
@@ -44,6 +82,7 @@ module.exports = (function() {
     };
 
     Superjoin.prototype.addModule = function(file) {
+        console.log('ADD', file);
         file = this.resolve(file);
         var module = 'require.register(\'' + (
             file.filename ? file.name + '\', \'' + file.filename : file.name
@@ -73,6 +112,7 @@ module.exports = (function() {
     };
 
     Superjoin.prototype.grepSubmodules = function(module, source) {
+        console.log('GREP', module);
         var pattern = /require\((.+?)\)/g,
             out = '';
         while(true) {
@@ -97,6 +137,10 @@ module.exports = (function() {
             split.pop();
             split = split.concat(subModule.split('/'));
             var newPath = [];
+            if (module.prefix) {
+                newPath.push(module.prefix);
+            }
+
             for (var i = 0, len = split.length; i < len; i++) {
                 if (split[i] === '..') {
                     newPath.pop();
@@ -121,14 +165,135 @@ module.exports = (function() {
         return grunt.file.read(file);
     };
 
+    Superjoin.prototype.loadModule = function(moduleType, file) {
+        var moduleDir,
+            modulePrefix;
+
+        switch (moduleType) {
+            case 'lib':
+                moduleDir = this.libDir;
+                modulePrefix = '$lib';
+                break;
+            case 'bower':
+                moduleDir = this.bwrDir;
+                modulePrefix = '$bower';
+                break;
+            case 'npm':
+                moduleDir = this.npmDir;
+                modulePrefix = '$npm';
+                break;
+            default:
+                throw new Error('Unknowd module type ' + moduleType + '!');
+        }
+
+        console.log('PATH', moduleDir, file);
+        var nodeModule = path.join(moduleDir, file),
+            name = file,
+            filepath,
+            filename;
+
+        var dir = nodeModule;
+        
+        if (name.indexOf('/') !== -1) {
+            filepath = nodeModule;
+            if (!/\.js(on)?$/.test(name)) {
+                name += '.js';
+                filepath += '.js';
+            }
+        }
+        else if (grunt.file.exists(nodeModule)) {
+            var bwr = grunt.file.exists(path.join(nodeModule, '/bower.json')) ?
+                require(path.join(nodeModule, '/bower.json')) : null;
+            var pkg = grunt.file.exists(path.join(nodeModule, '/package.json')) ?
+                require(path.join(nodeModule, '/package.json')) : null;
+            
+            if (bwr) {
+                filename = bwr.main;
+                if (Array.isArray(bwr.main)) {
+                    for (var i = 0, len = bwr.main.length; i < len; i++) {
+                        if (/\.js$/.test(bwr.main[i])) {
+                            filename = bwr.main[i];
+                            break;
+                        }
+                    }
+                }
+
+                filepath = path.join(nodeModule, filename);
+                filename = name + filepath.replace(dir, '');
+            }
+            else if (pkg && pkg.browser) {
+                filename = pkg.browser;
+                filepath = path.join(nodeModule, filename);
+                filename = name + filepath.replace(dir, '');
+            }
+            else if (pkg && pkg.main) {
+                filename = pkg.main;
+                filepath = path.join(nodeModule, filename);
+                filename = name + filepath.replace(dir, '');
+            }
+            else if (pkg) {
+                filename = 'index.js';
+                filepath = path.join(nodeModule, filename);
+                filename = name + filepath.replace(dir, '');
+            }
+            else {
+                throw new Error('No bower.json or package.json found in module ' + name + '!');
+            }
+        } else {
+            return null;
+        }
+
+        return {
+            name: name,
+            dir: dir,
+            path: filepath,
+            isNodeModule: true,
+            isModule: true,
+            prefix: modulePrefix
+        };
+    };
+
     Superjoin.prototype.resolve = function(file) {
+        console.log('Files', file);
+
         var filepath,
             name = file,
             filename,
             isNodeModule = false,
-            dir;
+            dir,
+            resolved;
 
-        if (file.charAt(0) === '.') {
+        if (/^\.?[a-zA-Z0-9_-]+$/.test(file)) {
+            console.log('Load from <modules>');
+            if (this.libDir) {
+                resolved = this.loadModule('lib', file);
+            }
+
+            if (!resolved) {
+                resolved = this.loadModule('bower', file);
+            }
+            
+            if (!resolved) {
+                resolved = this.loadModule('npm', file);
+            }
+
+            if (!resolved) {
+                throw new Error(
+                    'Module ' + file + ' not found! Use npm or bower to install it!\n' +
+                    ' npm install ' + file + ' --save\n' +
+                    ' bower install ' + file + '\n');
+            }
+        }
+        else if (file.charAt(0) === '$') {
+            var pos = file.indexOf('/');
+            var type = file.slice(1, pos);
+            file = file.substr(pos + 1);
+
+            console.log('TYPE', type, file);
+            resolved = this.loadModule(type, file);
+
+        }
+        else if (file.charAt(0) === '.') {
             if (!/\.js(on)?$/.test(file)) {
                 name += '.js';
             }
@@ -141,12 +306,13 @@ module.exports = (function() {
             filepath = name;
         }
         else {
-            var modulesDir = path.join(this.root, 'node_modules');
+            console.log('OTHER', file);
+            var modulesDir = path.join(this.root, this.libDir);
             while(true) {
                 if (!fs.existsSync(modulesDir)) {
-                    modulesDir = path.join(modulesDir, '../../node_modules');
-                    if (modulesDir === '/node_modules') {
-                        throw new Error('No node_modules folder found!');
+                    modulesDir = path.join(modulesDir, '../../', this.libDir);
+                    if (modulesDir === '/' + this.libDir) {
+                        throw new Error('No ' + this.libDir + ' folder found!');
                     }
                 }
                 else {
@@ -166,8 +332,24 @@ module.exports = (function() {
                 }
             }
             else if (grunt.file.exists(nodeModule)) {
+                var bwr = require(path.join(nodeModule, '/bower.json'));
                 var pkg = require(path.join(nodeModule, '/package.json'));
-                if (pkg.browser) {
+                
+                if (grunt.file.exists(bwr)) {
+                    filename = bwr.main;
+                    if (Array.isArray(bwr.main)) {
+                        for (var i = 0, len = bwr.main.length; i < len; i++) {
+                            if (/\.js$/.test(bwr.main[i])) {
+                                filename = bwr.main[i];
+                                break;
+                            }
+                        }
+                    }
+
+                    filepath = path.join(nodeModule, filename);
+                    filename = name + filepath.replace(dir, '');
+                }
+                else if (pkg.browser) {
                     filename = pkg.browser;
                     filepath = path.join(nodeModule, filename);
                     filename = name + filepath.replace(dir, '');
@@ -183,11 +365,14 @@ module.exports = (function() {
                     filename = name + filepath.replace(dir, '');
                 }
             } else {
-                throw new Error('Module ' + file + ' not found! Use npm install ' + file + ' --save to install it.');
+                throw new Error(
+                    'Module ' + file + ' not found! Use npm or bower to install it!\n' +
+                    ' npm install ' + file + ' --save\n' +
+                    ' bower install ' + file + '\n');
             }
         }
 
-        var resolved = {
+        resolved = resolved || {
             name: name,
             path: filepath,
             isNodeModule: isNodeModule
@@ -201,14 +386,18 @@ module.exports = (function() {
             resolved.filename = filename;
         }
 
+        console.log('Resolved:', resolved);
+
         return resolved;
     };
 
     Superjoin.prototype.getConf = function() {
         var confFiles = this.confFiles.length === 0 ? [
-                path.join(this.root, 'superjoin.json'),
-                path.join(this.root, 'package.json')
+                path.join(process.cwd(), 'superjoin.json'),
+                path.join(process.cwd(), 'package.json')
             ] : this.confFiles;
+
+        var cnf;
 
         for (var i = 0, len = confFiles.length; i < len; i++) {
             var file = confFiles[i];
@@ -220,13 +409,24 @@ module.exports = (function() {
                 }
                 
                 if (/\/package.json$/.test(file)) {
-                    var pkg = require(file);
+                    var pkg = grunt.file.readJSON(file);
                     if (pkg && pkg.superjoin) {
-                        return pkg.superjoin;
+                        cnf = pkg.superjoin;
+                        if (!/^\.{0,2}\//.test(cnf.main)) {
+                            cnf.main = './' + cnf.main;
+                        }
+
+                        return cnf;
                     }
                 }
 
-                return require(file);
+                var sjc = grunt.file.readJSON(file);
+                if (sjc) {
+                    if (!/^\.{0,2}\//.test(sjc.main)) {
+                        sjc.main = './' + sjc.main;
+                    }
+                }
+                return sjc;
             }
         }
 
